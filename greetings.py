cb86f4dfa592852a90d77134e9ece9784d606893
Sviatoslav Bordovski <sbordovski@gmail.com>
2019-08-31 10:47:02 +0300

    Create greetings.py

---
+++ greetings.py
@@ -0,0 +1,10 @@
+def greetings(language): #function that is just remembered by python
+    if language == 'es':
+        print('Hola, muchachos! =)')
+    elif language == 'fr':
+        print('Bonjour, musie!')
+    else:
+        print('GO AWAY!')
+# end of function that is recorded by python
+
+greetings('fr') # invoke of function depending on chosen 'es'/'fr'/'other' language
