a0ff7d3e4d59210536d0e61af30a3aea86346204
Sviatoslav Bordovski <sbordovski@gmail.com>
2019-09-02 13:30:40 +0300

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -8,4 +8,3 @@ def greetings(language): #function that is just remembered by python
 # end of function that is recorded by python
 
 greetings('fr') # invoke of function depending on chosen 'es'/'fr'/'other' language
- 
